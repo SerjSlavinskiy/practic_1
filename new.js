9e63a66bcdfc66fac23588ab510c4a5ce6a48bdb
Serhiy <rewritesiroj@gmail.com>
2026-01-12 07:10:39 +0200

    mywork_1

--- new.js
+++ new.js
@@ -1,7 +1,29 @@
 'use strict'
 
-if (4 == 9) {
-    console.log ('Ok!');
-} else {
-    console.log('Error');
-}
+// if (4 == 9) {
+//     console.log ('Ok!');
+// } else {
+//     console.log('Error');
+// }
+
+const dorkdroch = +prompt('Скільки разів за життя Ви дрочили?', '');
+
+const whenYouDrochDB = {
+    count: dorkdroch,
+    drochka_L: {},
+    drochka_R: {},
+    limbs: [],
+    privat: false
+};
+
+const a = prompt('Чи любите Ви дрочити лівою рукою?', ''),
+      b = prompt('Як Ви оціните дрочку лівою рукою?', ''),
+      c = prompt('Чи любите Ви дрочити правою рукою?', ''),
+      d = prompt('Як Ви оціните ддрочку правою рукою?', ''),
+      e = prompt('Якими частинами тіла Ви дрочили?', '', '', '', '');
+
+      whenYouDrochDB.drochka_L[a] = b;
+      whenYouDrochDB.drochka_R[c] = d;
+      whenYouDrochDB.limbs[e];
+      
+console.log(whenYouDrochDB);
